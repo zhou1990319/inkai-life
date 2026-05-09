7adf7844952b5a89dc23233f3a76628034aee473
zhou1990319 <309775870@qq.com>
2026-05-09 12:51:29 +0800

    Update server/index.js

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '../dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
